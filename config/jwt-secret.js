196de6420267a68338ac8d8a055090c685983b2c
Samira Aghabayova <aghabayova.samira@gmail.com>
2020-11-16 18:54:25 -0500

    fix: jwt

--- config/jwt-secret.js
+++ config/jwt-secret.js
@@ -1,5 +1,10 @@
 require('dotenv').config();
 
+// module.exports = {
+//  JWT_SECRET: (process.env.NODE_ENV !== 'production') ? 'JWT_SECRET' : process.env.JWT_SECRET,
+// };
+
+const { JWT_SECRET = 'wtf' } = process.env;
 module.exports = {
-  JWT_SECRET: (process.env.NODE_ENV !== 'production') ? 'JWT_SECRET' : process.env.JWT_SECRET,
+  JWT_SECRET,
 };
